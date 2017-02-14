a1db7a78184159242fab829486fde19b752d3123
pdhoward <patrick.howard@hotmail.com>
2017-02-14 11:22:28 -0500

    updates

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 
 'use strict';
+
+require('dotenv').config()
 ///////////////////////////////////////////////////////////
 ////////  chaoticreach universal channel integrator ///////
 ///////            version 0.5.0                   ///////
 /////////////////////////////////////////////////////////
 var botBuilder =      require('claudia-bot-builder');
-var greeting =        require('greeting');
 var fetch =           require('node-fetch');
 
 module.exports = botBuilder(function(message) {
-  if (message.text.match(/quote/i)) {
-     return fetch('http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en').then(function(res) {
+   return fetch('http://chaotic.ngrok.io/api/message').then(function(res) {
        return res.text();
      });
-   } else {
-     return greeting.random();
-   }
 });
-
-// module.exports = bot;
-
-/*
-
-const excuse = require('huh');
-
-module.exports = botBuilder(request =>
-  `Thank you for sending your request to ${request.text}. Your message is very important to us. The problem was caused by ${excuse.get()}`
-);
-*/
