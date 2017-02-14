7863eae138f1667f11868242781473b75089ad70
pdhoward <patrick.howard@hotmail.com>
2017-02-14 13:45:11 -0500

    updates

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 
 'use strict';
-
-require('dotenv').config()
 ///////////////////////////////////////////////////////////
 ////////  chaoticreach universal channel integrator ///////
 ///////            version 0.5.0                   ///////
 /////////////////////////////////////////////////////////
 var botBuilder =      require('claudia-bot-builder');
-var fetch =           require('node-fetch');
+var axios =           require('axios');
 
 module.exports = botBuilder(function(message) {
 
-   return fetch('http://chaotic.ngrok.io/api/message', { method: 'POST', body: message})
+   return axios.post('http://chaotic.ngrok.io/api/message',
+                { body: message })
                 .then(function(res) {
                     return res.text();
               });
